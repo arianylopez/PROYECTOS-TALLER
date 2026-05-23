596b68c818e1c83bc862e94b393ceaec8a365ba3
Ariany Lopez <arianylopez341@gmail.com>
2026-05-23 10:33:35 -0400

    (backend): Prueba de Servicio

--- HotelReservaAPI/PruebasHotel/EstadiaServiceTest.cs
+++ HotelReservaAPI/PruebasHotel/EstadiaServiceTest.cs
@@ -1,18 +1,70 @@
-﻿using HotelReservaAPI.Models;
-using HotelReservaAPI.Repositories;
-using HotelReservaAPI.Services;
-using Moq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+using HotelReservaAPI.Models;
+using HotelReservaAPI.Repositories;
+using HotelReservaAPI.Services;
+using Supabase;
+
 
 namespace HotelReservaAPI.Tests
 {
     [TestFixture]
-    public class EstadiaServiceTest
+    public class EstadiaServiceTests
     {
-        
+        [Test]
+        public void CrearReserva_DatosValidos_DebeRegistrarReservaCorrectamente()
+        {
+            var mockEstadiaRepo = new Mock<IEstadiaRepository>();
+            var mockHabitacionRepo = new Mock<IHabitacionRepository>();
+            var mockPoliticaRepo = new Mock<IPoliticaCancelacionRepository>();
+
+            var estadiaInput = new Estadia
+            {
+                HabitacionId = "1",
+                FechaIngreso = DateTime.Now.AddDays(1),
+                FechaSalida = DateTime.Now.AddDays(5),
+                CantidadPersonas = 2
+            };
+
+            var habitacionSimulada = new Habitacion
+            {
+                TipoHabitacionId = "10"
+            };
+
+            var tipoHabitacionSimulada = new TipoHabitacion
+            {
+                TipoHabitacionId = 1,
+                Capacidad = 4,
+                PrecioBase = 5000
+            };
+
+            mockHabitacionRepo.Setup(repo => repo.ObtenerHabitacionPorId(estadiaInput.HabitacionId))
+                              .Returns(habitacionSimulada);
+
+            mockHabitacionRepo.Setup(repo => repo.ObtenerTipoHabitacionPorId(habitacionSimulada.TipoHabitacionId))
+                              .Returns(tipoHabitacionSimulada);
+
+            mockEstadiaRepo.Setup(repo => repo.Insertar(It.IsAny<Estadia>()))
+                           .Returns((Estadia e) => e);
+
+            var servicio = new EstadiaService(
+                mockEstadiaRepo.Object,
+                mockHabitacionRepo.Object,
+                mockPoliticaRepo.Object,
+                null
+            );
+
+            var resultado = servicio.CrearReserva(estadiaInput);
+
+            Assert.That(resultado, Is.Not.Null);
+            Assert.That(resultado.Estado, Is.EqualTo("Reservada"));
+            Assert.That(resultado.PrecioAplicado, Is.EqualTo(5000));
+            Assert.That(resultado.Mora, Is.EqualTo(0));
+            Assert.That((DateTime.Now - resultado.FechaCreacion).TotalSeconds, Is.LessThan(5));
+            mockEstadiaRepo.Verify(repo => repo.Insertar(It.IsAny<Estadia>()), Times.Once);
+        }
     }
 }
